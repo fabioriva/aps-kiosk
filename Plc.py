4e4a95bcbc06e935ae036e4d978767f5fdec4644
Fabio Riva <f.riva@sotefin.ch>
2024-10-16 11:16:26 +0200

    Add files via upload

---
+++ Plc.py
@@ -0,0 +1,71 @@
+import snap7
+import time
+from socketify import OpCode
+
+CHANNEL = "/api/kiosk/info"
+IP = "192.168.20.55"
+POLL = 0.5
+RACK = 0
+SLOT = 1
+
+
+class Plc():
+    def __init__(self, app):
+        self.app = app
+        self.client = snap7.client.Client()
+        self.online = False
+
+    def connect(self):
+        try:
+            self.client.connect(IP, RACK, SLOT)
+            self.online = self.client.get_connected()
+        except Exception as e:
+            self.err(e)
+
+    def err(self, err):
+        print("exception error:", err)
+        e = self.client.get_last_error()
+        print("snap7 error:", e, self.client.error_text(e))
+        self.online = self.client.disconnect()
+
+    def read(self, db, start, size):
+        try:
+            buffer = self.client.db_read(db, start, size)
+            return {
+                "comm": self.online,
+                "lang": int.from_bytes(buffer[0:2], "big"),
+                "page": int.from_bytes(buffer[2:4], "big"),
+                "card": int.from_bytes(buffer[4:6], "big"),
+                "digitNr": int.from_bytes(buffer[6:8], "big"),
+                "errMesg": int.from_bytes(buffer[8:10], "big"),
+                "successMesg": int.from_bytes(buffer[10:12], "big")
+            }
+        except Exception as e:
+            self.err(e)
+
+    def write(self, area, dbNr, start, buffer):
+        try:
+            # print(area, dbNr, start, buffer)
+            # res = self.client.db_write(dbNr, start, buffer)
+            res = self.client.write_area(area, dbNr, start, buffer)
+            return res
+        except Exception as e:
+            self.err(e)
+
+    def run(self):  # , f):
+        self.connect()
+        data = dict()
+        while 1:
+            if (self.online):
+                data = self.read(37, 2, 12)
+                # watchdog DB37.DBX14.0
+                self.write(snap7.Area.DB, 37, 14, bytearray([0b00000001]))
+            else:
+                data = dict(comm=False, lang=0, page=0, card=0,
+                            digitNr=0, errMesg=0, succesMsg=0)
+                self.connect()
+                print(
+                    "Connected to PLC %s" % IP if self.online else "Connecting to PLC %s...." % IP)
+
+            self.app.publish(CHANNEL, data, opcode=OpCode.TEXT, compress=False)
+            time.sleep(POLL)
---
+++ app.py
@@ -0,0 +1,65 @@
+from Plc import Plc
+from snap7 import Area
+from socketify import App, AppOptions, OpCode, CompressOptions
+from threading import Thread
+
+PATH = "/api/kiosk"
+PORT = 9999
+
+
+async def pin(res, req):
+    json = await res.get_json()
+    pin = int(json["pin"], 16)
+    buf = bytearray(pin.to_bytes(2, byteorder='big'))
+    plc.write(Area.DB, 37, 16, buf)
+    res.cork_end(json)
+
+
+def press_button(res, req):
+    plc.write(Area.DB, 37, 15, bytearray([1]))
+    res.end({"message": "press button"})
+
+
+def release_button(res, req):
+    plc.write(Area.DB, 37, 15, bytearray([0]))
+    res.end({"message": "release button"})
+
+
+def ws_open(ws):
+    print('A WebSocket got connected!')
+    # ws.send("Hello World!", OpCode.TEXT)
+    ws.subscribe(PATH + '/info')
+
+
+def ws_message(ws, message, opcode):
+    # Ok is false if backpressure was built up, wait for drain
+    # ok = ws.send(message, opcode)
+    ws.publish(CHANNEL, message, opcode)
+
+
+def make_app(app, plc):
+    app.ws("/*", {
+        'compression': CompressOptions.SHARED_COMPRESSOR,
+        'max_payload_length': 16 * 1024 * 1024,
+        'idle_timeout': 0,
+        'open': ws_open,
+        'message': ws_message,
+        'drain': lambda ws: print('WebSocket backpressure: %i' % ws.get_buffered_amount()),
+        'close': lambda ws, code, message: print('WebSocket closed')
+    })
+    app.get(PATH + "/press", press_button)
+    app.get(PATH + "/unpress", release_button)
+    app.post(PATH + "/pin", pin)
+    app.any("/*", lambda res, req: res.write_status(404).end("Not Found"))
+    # S7 comm
+    thread = Thread(target=plc.run, daemon=True)
+    thread.start()
+
+
+if __name__ == "__main__":
+    app = App()
+    plc = Plc(app)
+    make_app(app, plc)
+    app.listen(PORT, lambda config: print(
+        "Listening on port http://localhost:%d now\n" % (config.port)))
+    app.run()
